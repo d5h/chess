c41a1b1d982f2d40d16476f90b77431dce5069bb
Dan Hipschman <d5h@users.noreply.github.com>
2022-07-05 18:33:54 -0700

    Change colors

--- src/main.rs
+++ src/main.rs
@@ -56,14 +56,16 @@ impl Game {
     }
 
     fn draw_board(&self) {
-        clear_background(BEIGE);
+        let light = Color::new(0.93, 1.0, 0.98, 1.0);
+        let dark = Color::new(0.4, 0.7, 0.7, 1.0);
+        clear_background(light);
         for r in 0..8 {
             // TODO: get board size from rules
             for c in 0..8 {
                 if (r + c) % 2 == 1 {
                     let y = r as f32 * SQUARE_SIZE;
                     let x = c as f32 * SQUARE_SIZE;
-                    draw_rectangle(x, y, SQUARE_SIZE, SQUARE_SIZE, BROWN);
+                    draw_rectangle(x, y, SQUARE_SIZE, SQUARE_SIZE, dark);
                 }
             }
         }
